7d91408de26fee18c745f9bfd534dbb2a78fcd34
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-06-30 09:33:33 +0200

    Update interfaces.tsx

--- src/utils/interfaces.tsx
+++ src/utils/interfaces.tsx
@@ -1,19 +1,60 @@
-import {ReactNode} from 'react'
+import { ReactNode } from 'react'
 
+/* Define the props for the Button component */
 interface ButtonProps {
   variant: 'orange' | 'black' | 'border';
   children: ReactNode;
   product?: {
-      id: number;
-      name: string;
-      price: number;
-      image: string;
-      quantity: number;
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+    quantity: number;
   };
 }
 
+/* Define the structure for a Product object */
 interface Product {
-    id: number;
+  id: number;
+  slug: string;
+  name: string;
+  image: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  category: string;
+  categoryImage: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  new: boolean;
+  price: number;
+  description: string;
+  features: string;
+  includes: Array<{
+    quantity: number;
+    item: string;
+  }>;
+  gallery: {
+    first: {
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+    second: {
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+    third: {
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+  };
+  others: Array<{
     slug: string;
     name: string;
     image: {
@@ -21,46 +62,7 @@ interface Product {
       tablet: string;
       desktop: string;
     };
-    category: string;
-    categoryImage: {
-      mobile: string;
-      tablet: string;
-      desktop: string;
-    };
-    new: boolean;
-    price: number;
-    description: string;
-    features: string;
-    includes: Array<{
-      quantity: number;
-      item: string;
-    }>;
-    gallery: {
-      first: {
-        mobile: string;
-        tablet: string;
-        desktop: string;
-      };
-      second: {
-        mobile: string;
-        tablet: string;
-        desktop: string;
-      };
-      third: {
-        mobile: string;
-        tablet: string;
-        desktop: string;
-      };
-    };
-    others: Array<{
-      slug: string;
-      name: string;
-      image: {
-        mobile: string;
-        tablet: string;
-        desktop: string;
-      };
-    }>;
-  }
+  }>;
+}
 
-export type {ButtonProps, Product}
+export type { ButtonProps, Product }
